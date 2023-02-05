4395645e7ab937398397c9e69599a8f51875abf2
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-02-05 22:16:10 +0400

    add first page preview functionality

--- js/index.js
+++ js/index.js
@@ -4,6 +4,17 @@ const lname = document.querySelector("#lname");
 const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const twoGeorgians = document.querySelectorAll(".twoGeorgian");
+const fileInput = document.querySelector("#file");
+const aboutText = document.querySelector("#about");
+const aboutTextTitle = document.querySelector(".pre-about");
+
+// PREVIEW ELEMENTS
+const preImage = document.querySelector(".pre-image");
+const preName = document.querySelector(".pre-fname");
+const preLastName = document.querySelector(".pre-lname");
+const preEmail = document.querySelector(".pre-email");
+const prePhone = document.querySelector(".pre-phone");
+const preAboutText = document.querySelector(".pre-about-text");
 
 // REGEX FOR VALIDATION
 const cr_twoGeorgian = /^[ა-ჰ]{2,}$/;
@@ -16,8 +27,9 @@ function validateInput(input, criteria) {
 }
 
 twoGeorgians.forEach((input) => {
-	input.addEventListener("input", () => {
+	input.addEventListener("input", (i) => {
 		if (!validateInput(input, cr_twoGeorgian)) {
+			phone.parentElement.classList.remove("successful");
 			input.parentElement.classList.add("danger");
 		} else {
 			input.parentElement.classList.remove("danger");
@@ -26,7 +38,20 @@ twoGeorgians.forEach((input) => {
 	});
 });
 
+fname.addEventListener("input", () => {
+	preName.innerHTML = fname.value;
+});
+
+lname.addEventListener("input", () => {
+	preLastName.innerHTML = lname.value;
+});
+
 email.addEventListener("input", () => {
+	if (email.value.length > 0) {
+		preEmail.previousElementSibling.classList.remove("hidden");
+	}
+	preEmail.innerHTML = email.value;
+
 	if (!validateInput(email, cr_mail)) {
 		email.parentElement.classList.add("danger");
 	} else {
@@ -36,10 +61,27 @@ email.addEventListener("input", () => {
 });
 
 phone.addEventListener("input", () => {
+	if (phone.value.length > 0) {
+		prePhone.previousElementSibling.classList.remove("hidden");
+	}
+	prePhone.innerHTML = phone.value;
 	if (!validateInput(phone, cr_phone)) {
 		phone.parentElement.classList.add("danger");
 	} else {
 		phone.parentElement.classList.remove("danger");
 		phone.parentElement.classList.add("successful");
 	}
+});
+
+fileInput.addEventListener("change", () => {
+	const file = fileInput.files[0];
+	const objectUrl = URL.createObjectURL(file);
+	preImage.src = objectUrl;
+});
+
+aboutText.addEventListener("input", () => {
+	if (aboutText.value.length > 0) {
+		aboutTextTitle.classList.remove("hidden");
+	}
+	preAboutText.innerHTML = aboutText.value;
 });
